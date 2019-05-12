46f1b5c71bc6942d091ba3b82d3d68edc6561f65
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-05-12 09:31:47 +0300

    puhelinluettelo steps 11 + 12

--- osa2/puhelinluettelo/src/app.js
+++ osa2/puhelinluettelo/src/app.js
@@ -3,16 +3,28 @@ import personService from './person-service'
 import PersonFilter from './person-filter'
 import NewPersonForm from './new-person-form'
 import PersonList from './person-list'
+import Notification from './notification'
 
 const App = () => {
   const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
   const [ filterText, setFilterText ] = useState('')
+  const [ notification, setNotification ] = useState({ msg: '', style: null })
 
   useEffect(() => {
     personService.list().then(persons => setPersons(persons))
   }, [])
+
+  const showNotification = msg => {
+    setNotification({ msg: msg, style: null })
+    setTimeout(() => setNotification({ msg: '', style: null }), 2000)
+  }
+
+  const showError = msg => {
+    setNotification({ msg: msg, style: 'error' })
+    setTimeout(() => setNotification({ msg: '', style: null }), 2000)
+  }
 
   const addNewPerson = event => {
     event.preventDefault()
@@ -21,16 +33,19 @@ const App = () => {
       if (window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero uudella?`)) {
         personService
           .update(existingPerson.id, { name: newName, number: newPhone })
-          .then(updatedPerson =>
+          .then(updatedPerson => {
             setPersons(persons.map(person =>
               person.id === updatedPerson.id ? updatedPerson : person 
             ))
-          )
+            showNotification(`Muutettiin henkilön ${newName} puhelinnumeroa`)
+          })
       }
     } else {
       personService
         .add({ name: newName, number: newPhone })
         .then(newPerson => setPersons(persons.concat(newPerson)))
+        .catch(error => showError(`${newName} on jo poistettu`))
+      showNotification(`Lisättiin ${newName}`)
       setNewName('')
       setNewPhone('')
     }
@@ -38,9 +53,10 @@ const App = () => {
 
   const removePerson = personToBeRemoved => {
     if (window.confirm(`Poistetaanko ${personToBeRemoved.name}?`)) {
-      personService.remove(personToBeRemoved.id).then(() =>
+      personService.remove(personToBeRemoved.id).then(() => {
         setPersons(persons.filter(person => person.id !== personToBeRemoved.id))
-      )
+        showNotification(`Poistettu ${personToBeRemoved.name}`)
+      }).catch(error => showError(`${personToBeRemoved.name} on jo poistettu`))
     }
   }
 
@@ -51,6 +67,7 @@ const App = () => {
   return (
     <div>
       <h1>Puhelinluettelo</h1>
+      <Notification message={notification.msg} style={notification.style} />
       <PersonFilter filterText={filterText} filter={(e) => filter(e)} />
       <h2>lisää uusi</h2>
       <NewPersonForm
